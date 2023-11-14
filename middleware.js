7bc8340b740f34e6de4c8c9d1fff6d811ef81327
mrgabodev <gabriel.escobar.torres@gmail.com>
2023-11-13 23:48:11 -0600

    fix: :lock: recover middleware role-base routing

--- middleware.js
+++ middleware.js
@@ -1,14 +1,18 @@
-import { withAuth } from 'next-auth/middleware'
+import { withAuth } from "next-auth/middleware"
 
 export default withAuth({
-  callbacks: {
-    authorized: ({ req, token }) => {
-
-      return true
-    }
-  }
-})
-
+    callbacks: {
+      authorized: ({req, token }) => {
+        if(req.nextUrl.pathname.startsWith('/admin')){
+          return token?.role === 'admin'
+        }
+        if(req.nextUrl.pathname.startsWith('/graduate')){
+          return token?.role === 'graduate'
+        }
+        return false
+      },
+    },
+  })
 export const config = {
   matcher: ['/admin/:path*', '/graduate/:path*']
 }
