d8d2f929b5c70c94a4c0a934f3715ea9d26dd594
mrgabdev <gabriel.escobar.torres@gmail.com>
2023-12-04 04:18:38 -0600

    feat: :sparkles: add homepage for admin dashboard

--- app/(dashboard)/admin/dashboard/page.jsx
+++ app/(dashboard)/admin/dashboard/page.jsx
@@ -1,23 +1,50 @@
-'use client'
+import Image from 'next/image'
+import Typography from '@mui/material/Typography'
 
-import CircularProgressWithLabel from '@/components/CircularProgressWithLabel'
-import { useSession } from 'next-auth/react'
-
-const Dashboard = () => {
-  const { data: session, status } = useSession()
-
-  if (status === 'loading') return <CircularProgressWithLabel/>
-
-  console.log(session)
-
+export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <pre>
-        <code>{JSON.stringify(session?.user, null, 2)}</code>
-      </pre>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100%'
+      }}
+    >
+      <Typography
+        variant='h2'
+        fontSize={{
+          xs: '2.5em',
+          sm: '3em',
+          md: '4em'
+        }}
+        component='h1'
+        color='primary'
+      >
+        ¡Bienvenido!
+      </Typography>
+      <Typography variant='h5'>Comencemos</Typography>
+
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          maxWidth: '600px',
+          aspectRatio: '1/1'
+        }}
+      >
+        <Image
+          src='/images/hero-dashboard.svg'
+          alt='Bienvenido'
+          style={{
+            objectFit: 'contain'
+          }}
+          sizes='(max-width: 768px) 100vw, 600px'
+          fill
+          priority
+        />
+      </div>
     </div>
   )
 }
-
-export default Dashboard
