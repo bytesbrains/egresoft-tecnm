7d85cc390654828efe9f1aa46a96984dd588fee9
mrgabdev <gabriel.escobar.torres@gmail.com>
2023-12-15 08:54:07 -0600

    feat: 🚑️ fix store functions and fix client side

--- store/usePage.js
+++ store/usePage.js
@@ -1,25 +1,33 @@
-'use client'
-
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
-export const usePage = create(
-  persist(
-    (set) => ({
-      currentPage: sessionStorage.getItem('current-page') || 'Inicio',
-      currentTab: parseInt(sessionStorage.getItem('current-tab')) || 0,
-      setCurrentPage: (page) => {
-        set({ currentPage: page })
-        sessionStorage.setItem('current-page', page)
-      },
-      setCurrentTab: (index) => {
-        set({ currentTab: index })
-        sessionStorage.setItem('current-tab', index.toString())
-      }
-    }),
-    {
-      name: 'page-state',
-      getStorage: () => createJSONStorage(() => sessionStorage)
-    }
-  )
-)
+// Esta función crea un objeto de almacenamiento que usa sessionStorage
+const createSessionStorage = () => ({
+  getItem: (key) => sessionStorage.getItem(key),
+  setItem: (key, value) => sessionStorage.setItem(key, value),
+  removeItem: (key) => sessionStorage.removeItem(key)
+})
+
+export const usePage =
+  typeof window !== 'undefined'
+    ? create(
+        persist(
+          (set) => ({
+            currentPage: sessionStorage.getItem('current-page') || 'Inicio',
+            currentTab: parseInt(sessionStorage.getItem('current-tab')) || 0,
+            setCurrentPage: (page) => {
+              set({ currentPage: page })
+              sessionStorage.setItem('current-page', page)
+            },
+            setCurrentTab: (index) => {
+              set({ currentTab: index })
+              sessionStorage.setItem('current-tab', index.toString())
+            }
+          }),
+          {
+            name: 'page-state',
+            storage: createSessionStorage()
+          }
+        )
+      )
+    : null
