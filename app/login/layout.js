cd61e9085817106c33a761f4c0499620ed426f55
mrgabdev <gabriel.escobar.torres@gmail.com>
2023-12-15 10:48:04 -0600

    fix: 💩 add meta for inscure content load

--- app/login/layout.js
+++ app/login/layout.js
@@ -13,86 +13,92 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <Grid container sx={{ height: '100%' }}>
-      <Grid
-        item
-        xs={false}
-        sm={false}
-        md={6}
-        sx={{
-          backgroundImage: 'url(/images/TecNM.svg)',
-          backgroundRepeat: 'no-repeat',
-          backgroundColor: 'primary.main',
-          backgroundSize: 'contain',
-          backgroundPosition: 'left'
-        }}
+    <>
+      <meta
+        http-equiv='Content-Security-Policy'
+        content='upgrade-insecure-requests'
       />
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        md={6}
-        component={Paper}
-        elevation={6}
-        square
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: {
-            xs: 'start',
-            md: 'center'
-          }
-        }}
-      >
-        <Box
+      <Grid container sx={{ height: '100%' }}>
+        <Grid
+          item
+          xs={false}
+          sm={false}
+          md={6}
+          sx={{
+            backgroundImage: 'url(/images/TecNM.svg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: 'primary.main',
+            backgroundSize: 'contain',
+            backgroundPosition: 'left'
+          }}
+        />
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          component={Paper}
+          elevation={6}
+          square
           sx={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            justifyContent: 'center',
-            width: {
-              xs: '90%',
-              md: '70%'
+            justifyContent: {
+              xs: 'start',
+              md: 'center'
             }
           }}
         >
           <Box
             sx={{
-              display: {
-                xs: 'initial',
-                md: 'none'
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: {
+                xs: '90%',
+                md: '70%'
               }
             }}
           >
-            <Image
-              src='/images/tecnmMobile.svg'
-              alt='Tecnologico nacional de mexico'
-              width={150}
-              height={150}
-            />
+            <Box
+              sx={{
+                display: {
+                  xs: 'initial',
+                  md: 'none'
+                }
+              }}
+            >
+              <Image
+                src='/images/tecnmMobile.svg'
+                alt='Tecnologico nacional de mexico'
+                width={150}
+                height={150}
+              />
+            </Box>
+
+            <Typography
+              component='h2'
+              sx={{
+                mb: '30px',
+                textAlign: 'center',
+                typography: {
+                  xs: 'h4',
+                  md: 'h2'
+                }
+              }}
+            >
+              Egresoft TecNM
+            </Typography>
+
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            {children}
           </Box>
-
-          <Typography
-            component='h2'
-            sx={{
-              mb: '30px',
-              textAlign: 'center',
-              typography: {
-                xs: 'h4',
-                md: 'h2'
-              }
-            }}
-          >
-            Egresoft TecNM
-          </Typography>
-
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          {children}
-        </Box>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
